c4d6cf46adb838bb0c9daf8ecfa13a8105e1dc43
nash99 <ignacio99k@hotmail.com>
2023-04-20 12:04:18 -0400

    add: Ruta de generos/usuarios/id

--- src/controllers/generos.controllers.js
+++ src/controllers/generos.controllers.js
@@ -11,3 +11,20 @@ export const getGeneros = async (req,res) => {
         })
     }
 }
+
+export const getGenerosUsuarios = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('select * from usuario where idGenero_musical = ?',[req.params.id]);
+        console.log(req.params.id)
+        console.log('OBTENER USUARIOS POR GENERO MUSICAL')
+        if(rows.length <= 0) return res.status(404).json({
+            message: 'Usuario no encontrado'
+        })
+        res.json(rows[0]);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+
+}
--- src/routes/generos.routes.js
+++ src/routes/generos.routes.js
@@ -1,7 +1,8 @@
 import {Router} from 'express'
-import {getGeneros} from '../controllers/generos.controllers.js';
+import {getGeneros, getGenerosUsuarios} from '../controllers/generos.controllers.js';
 
 const router = Router();
 router.get('/generos', getGeneros);
+router.get('/generos/usuarios/:id', getGenerosUsuarios);
 
 export default router;
