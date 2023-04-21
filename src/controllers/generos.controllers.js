b983df05cb8bd57e6f65d6dea398940e2feea5d4
nash99 <ignacio99k@hotmail.com>
2023-04-20 20:49:55 -0400

    add: Inner join getGenerosUsuarios generos.controllers.js

--- src/controllers/generos.controllers.js
+++ src/controllers/generos.controllers.js
@@ -14,9 +14,11 @@ export const getGeneros = async (req,res) => {
 
 export const getGenerosUsuarios = async (req,res) => {
     try{
-        const [ rows ] = await pool.query('select * from usuario where idGenero_musical = ?',[req.params.id]);
+        
+        const [ rows ] = await pool.query('select usuario.idUsuario,usuario.nombreUsuario,usuario.apellidoUsuario,usuario.nickUsuario,usuario.emailUsuario, genero_musical.nombreGenero_musical from usuario inner join genero_musical on usuario.idGenero_musical = genero_musical.idGenero_musical where usuario.idGenero_musical = ?',[req.params.id]);
         console.log('OBTENER USUARIOS POR GENERO MUSICAL')
         console.log(rows)
+        console.log(rows.length)
         if(rows.length <= 0) return res.status(404).json({
             message: 'Usuario no encontrado'
         })
@@ -28,3 +30,4 @@ export const getGenerosUsuarios = async (req,res) => {
     }
 
 }
+
