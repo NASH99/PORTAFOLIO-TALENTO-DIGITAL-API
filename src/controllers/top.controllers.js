bc1115da2b870212ee5a6a73853510a61b20eb22
nash99 <ignacio99k@hotmail.com>
2023-04-22 11:29:57 -0400

    add: Nueva consulta inner join top.controllers.js

--- src/controllers/top.controllers.js
+++ src/controllers/top.controllers.js
@@ -2,7 +2,7 @@ import {pool} from '../db.js'
 
 export const getTop10 = async (req,res) => {
     try{
-        const [ rows ] = await pool.query('select * from usuario order by topUsuario desc limit 10');
+        const [ rows ] = await pool.query('select usuario.idUsuario,usuario.nombreUsuario,usuario.nickUsuario, genero_musical.nombreGenero_musical from usuario inner join genero_musical on usuario.idGenero_musical = genero_musical.idGenero_musical order by topUsuario desc limit 10;');
         console.log('OBTENER TOP USUARIOS')
         res.json(rows);
     } catch (error){
