f4b947477769435c607ccfa37599104d0d1aee34
nash99 <ignacio99k@hotmail.com>
2023-04-22 16:42:59 -0400

    fix: Consulta SQL en generos.controllers.js

--- src/controllers/generos.controllers.js
+++ src/controllers/generos.controllers.js
@@ -16,7 +16,7 @@ export const getGenerosUsuarios = async (req,res) => {
     const idGenero = req.params.id;
     console.log(idGenero);
     try{
-        const [ rows ] = await pool.query('select usuario.idUsuario,usuario.nombreUsuario,usuario.apellidoUsuario,usuario.nickUsuario,usuario.emailUsuario, genero_musical.nombreGenero_musical from usuario inner join genero_musical on usuario.idGenero_musical = genero_musical.idGenero_musical where usuario.idGenero_musical = ?',[req.params.id]);
+        const [ rows ] = await pool.query('SELECT U.idUsuario,U.nombreUsuario,U.apellidoUsuario,P.nombreArtistaPerfil, G.nombreGenero_musical FROM usuario U INNER JOIN genero_musical G ON U.idGenero_musical = G.idGenero_musical INNER JOIN perfil P ON U.idPerfil = P.idPerfil where U.idGenero_musical = ?',[req.params.id]);
         console.log('OBTENER USUARIOS POR GENERO MUSICAL')
         console.log(rows)
         console.log(rows.length)
