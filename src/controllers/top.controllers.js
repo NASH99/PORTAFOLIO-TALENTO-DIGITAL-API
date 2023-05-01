0a95f83a02f07b63cc42134960f8a23afaf1c503
nash99 <ignacio99k@hotmail.com>
2023-05-01 13:46:36 -0400

    add: Nuevas rutas Top

--- src/controllers/top.controllers.js
+++ src/controllers/top.controllers.js
@@ -12,3 +12,102 @@ export const getTop10 = async (req,res) => {
     }
 }
 
+export const getTopAfro = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 1  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopBlues = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 2  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopJazz = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 3  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopRyb = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 4  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopSalsa = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 5  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopRock = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 6  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopElectronica = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 7  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopReggaeton = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 8  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
+export const getTopPop = async (req,res) => {
+    try{
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,P.idGenero_musical,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = 9  limit 10;');
+        res.json(rows);
+    } catch (error){
+        return res.status(500).json({
+            message: 'Algo falló'
+        })
+    }
+}
+
--- src/routes/top.routes.js
+++ src/routes/top.routes.js
@@ -1,10 +1,18 @@
 import {Router} from 'express'
-import { getTop10 } from '../controllers/top.controllers.js';
+import { getTop10, getTopAfro, getTopBlues, getTopElectronica, getTopJazz, getTopPop, getTopReggaeton, getTopRock, getTopRyb, getTopSalsa } from '../controllers/top.controllers.js';
 
 const router = Router();
 
 //Creacion de rutas junto con su importacion de controlador
 router.get('/top10', getTop10);
-
+router.get('/topAfro', getTopAfro);
+router.get('/topBlues', getTopBlues);
+router.get('/topJazz', getTopJazz);
+router.get('/topR&B', getTopRyb);
+router.get('/topSalsa', getTopSalsa);
+router.get('/topRock', getTopRock);
+router.get('/topElectronica', getTopElectronica);
+router.get('/topReggaeton', getTopReggaeton);
+router.get('/topPop', getTopPop);
 
 export default router;
