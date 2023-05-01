41c83dd214c26911ca6fc4861bd56f714eef145b
nash99 <ignacio99k@hotmail.com>
2023-04-30 20:14:55 -0400

    fix: consultas sql

--- src/controllers/generos.controllers.js
+++ src/controllers/generos.controllers.js
@@ -16,7 +16,7 @@ export const getGeneros = async (req,res) => {
 export const getGenerosUsuarios = async (req,res) => {
     const idGenero = req.params.id;
     try{
-        const [ rows ] = await pool.query('SELECT * FROM imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN genero_musical G on P.idGenero_musical = G.idGenero_musical WHERE P.idGenero_musical = ?;',[req.params.id]);
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.idGenero_musical, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 having P.idGenero_musical = ?;',[req.params.id]);
         if(rows.length <= 0) return  res.status(404).json([{
             message: 'Usuario no encontrado',
             nombreGenero_musical: 'Genero'
--- src/controllers/top.controllers.js
+++ src/controllers/top.controllers.js
@@ -3,7 +3,7 @@ import {pool} from '../db.js'
 //Obtener top 10 usuarios con mayor puntuacion (topUsuario)
 export const getTop10 = async (req,res) => {
     try{
-        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.imagen from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical order by P.topUsuario desc limit 10;');
+        const [ rows ] = await pool.query('SELECT U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil,(SELECT imagen FROM imagen where idPerfil = I.idPerfil limit 1 ) as imagen2 from imagen I INNER JOIN perfil P ON I.idPerfil = P.idPerfil INNER JOIN usuario U ON P.idUsuario = U.idUsuario INNER JOIN genero_musical G ON P.idGenero_musical = G.idGenero_musical group by U.idUsuario, P.nombreArtistaPerfil,G.nombreGenero_musical ,P.topUsuario, I.idPerfil, imagen2 order by P.topUsuario desc limit 10;');
         res.json(rows);
     } catch (error){
         return res.status(500).json({
